e9ba5cc9cbcce5913dff97332f6b5b3ddb67876a
Luiz Henrique Cassettari <ricaun@users.noreply.github.com>
2021-12-07 17:09:29 -0300

    - Clear Build - Update Develop.yml `branches-ignore:`

--- Build/Build.cs
+++ Build/Build.cs
@@ -1,6 +1,5 @@
 using Nuke.Common;
 using Nuke.Common.Execution;
-using Nuke.Common.ProjectModel;
 using ricaun.Nuke;
 using ricaun.Nuke.Components;
 
@@ -10,26 +9,4 @@ partial class Build : NukeBuild, IPublishPack
     string IHazContent.Folder => "Release";
     string IHazRelease.Folder => "ReleasePack";
     public static int Main() => Execute<Build>(x => x.From<IPublishPack>().Build);
-}
-
-partial class Build2 : NukeBuild
-{
-    /*
-   public static int Main() => Execute<Build>(x => x.Compile);
-    */
-    [Solution] readonly Solution Solution;
-
-    Target Clean => _ => _
-        .Executes(() =>
-        {
-            Solution.ClearSolution(BuildProjectDirectory);
-        });
-
-    Target Compile => _ => _
-        .DependsOn(Clean)
-        .Executes(() =>
-        {
-            Solution.BuildMainProject();
-        });
-
 }
--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,8 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
 ## [1.0.3] / 2021-12-07
+- Clear Build
+- Update Develop.yml `branches-ignore:`
 - Add ricaun.Nuke
 
 ## [1.0.2] / 2021-12-05
