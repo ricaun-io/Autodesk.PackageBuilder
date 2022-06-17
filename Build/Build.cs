6ab136cbe8c7b19bf0fc285b716f8b79e8e91403
Luiz Henrique Cassettari <ricaun@users.noreply.github.com>
2022-06-17 13:38:19 -0300

    - Update to net6.0

--- Build/Build.cs
+++ Build/Build.cs
@@ -3,7 +3,6 @@ using Nuke.Common.Execution;
 using ricaun.Nuke;
 using ricaun.Nuke.Components;
 
-[CheckBuildProjectConfigurations]
 partial class Build : NukeBuild, IPublishPack, ICompileExample
 {
     public static int Main() => Execute<Build>(x => x.From<IPublishPack>().Build);
