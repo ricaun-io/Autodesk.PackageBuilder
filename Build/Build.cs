30d35c9dc790f113f908e06686f961b3f63410a5
Luiz Henrique Cassettari <ricaun@gmail.com>
2025-05-16 18:28:59 -0300

    Update `PrePack`

--- Build/Build.cs
+++ Build/Build.cs
@@ -3,7 +3,7 @@ using Nuke.Common.Execution;
 using ricaun.Nuke;
 using ricaun.Nuke.Components;
 
-partial class Build : NukeBuild, IPublishPack, ICompileExample, ITest
+partial class Build : NukeBuild, IPublishPack, IPrePack, ICompileExample, ITest
 {
     public static int Main() => Execute<Build>(x => x.From<IPublishPack>().Build);
 }
